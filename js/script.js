ea8034e564aeeebe5f6a028f9981b1073f2a8606
deemahs <s202354030@kfupm.edu.sa>
2026-09-19 18:33:17 +0300

    Start writing the js file (adding the dark and light mode button)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
+// ========================================
+// Select Elements
+// ========================================
 
+const themeButton =
+    document.getElementById("themeButton");
+
+const contactForm =
+    document.getElementById("contactForm");
+
+const formMessage =
+    document.getElementById("formMessage");
+
+const year =
+    document.getElementById("year");
+
+
+// ========================================
+// Dark / Light Mode
+// ========================================
+
+themeButton.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        themeButton.textContent = "☀️";
+
+    } else {
+
+        themeButton.textContent = "🌙";
+
+    }
+
+});
